e669330f87320c1f3cd292743148e4234b5ad9a1
Jordan Snow <jordapianistsnow@gmail.com>
2023-10-15 17:58:15 -0600

    Corrected test_TotalAges.py

--- test_TotalAges.py
+++ test_TotalAges.py
@@ -4,29 +4,22 @@ import src.total_ages as total_ages
 class TestTotalAges(unittest.TestCase):
     def input_replacement(self, prompt):
         self.inputed_prompts.append(prompt)
-        r = self.test_inputs[self.index][self.bin]
+        r = self.test_inputs[self.index]
         self.index += 1
-        self.bin +=1
-        if self.index >= len(self.test_inputs):
-            self.index = 0
-        if self.bin > 1:
-            self.bin = 0
         return r
     
     def print_replacement(self, *text):
-        line = "".join(text) + "\n"
+        line = " ".join(map(str, text)) + "\n"
         self.printed_lines.append(line)
-        print(text)
 
 
 
 
     def setUp(self):
         self.inputed_prompts = []
-        self.test_inputs = [["5","6"], ["7","67"],["88","34"]]
+        self.test_inputs = [5,7]
         self.printed_lines = []
         self.index = 0
-        self.bin = 0
         total_ages.print = self.print_replacement
         total_ages.input = self.input_replacement
     
@@ -55,22 +48,9 @@ class TestTotalAges(unittest.TestCase):
         self.bin = 0
     
     def test_003TotalAgesOutputs(self):
-        '''
-        Prelude
-        '''
-        expected = []
-        for i in range(len(self.test_inputs)):
-            total = int(self.test_inputs[i][0]) + int(self.test_inputs[i][1])
-            expected.append(str(total) + "\n" )
-            total_ages.TotalAges()
-        '''
-        Running tests
-        '''
-        self.assertListEqual(self.printed_lines, expected)
-
-        '''
-        Postlude
-        '''
+        expected = ["12\n"]
+        total_ages.TotalAges()
+        self.assertListEqual(expected, self.printed_lines)
 
 if __name__ == "__main__":
     unittest.main()
